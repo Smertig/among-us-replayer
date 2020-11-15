850069913696b8232c89a0b746311a406c909e75
Smertig <akaraevz@mail.ru>
2020-11-15 16:49:16 +0300

    Fix render for linux

--- source/util/platform.cpp
+++ source/util/platform.cpp
@@ -12,6 +12,10 @@
 #include <Windows.h>
 #include <array>
 
+void prepare_render_impl() {
+    // nothing
+}
+
 void show_warning_impl(std::string_view text) {
     MessageBoxA(nullptr, std::string(text).c_str(), platform::get_app_fullname().c_str(), MB_ICONWARNING);
 }
@@ -26,6 +30,12 @@ std::filesystem::path get_self_path_impl() {
 #elif defined(__linux__)
 
 #include <fmt/color.h>
+
+#include <X11/Xlib.h>
+
+void prepare_render_impl() {
+    XInitThreads();
+}
 
 void show_warning_impl(std::string_view text) {
     fmt::print(fg(fmt::color::orange_red), "{}", text);
@@ -63,6 +73,8 @@ void show_warning(std::string_view text) {
 }
 
 void on_app_start() {
+    prepare_render_impl();
+
     // Try fix paths
     if (!std::filesystem::exists("res/config.json")) {
         fix_working_directory();
